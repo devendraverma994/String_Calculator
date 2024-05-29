fdc343726b5e6092f163214479748ac48ef41b59
Devendra Verma <devendraverma994@gmail.com>
2024-05-29 15:02:47 +0530

    add test cases for model

---
+++ spec/models/string_calculatore_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+RSpec.describe StringCalculator, type: :model do
+  describe "#add" do
+    context "with empty input" do
+      it "returns 0" do
+        calculator = StringCalculator.new
+        expect(calculator.add("")).to eq(0)
+      end
+    end
+
+    context "with positive numbers" do
+      it "returns the sum of the numbers" do
+        calculator = StringCalculator.new
+        expect(calculator.add("1,2,3")).to eq(6)
+      end
+    end
+
+    context "with negative numbers" do
+      it "raises an error" do
+        calculator = StringCalculator.new
+        expect { calculator.add("1,-2,3") }.to raise_error("Negative numbers not allowed: -2")
+      end
+    end
+
+    context "with custom delimiters" do
+      it "returns the sum using custom delimiters" do
+        calculator = StringCalculator.new
+        expect(calculator.add("//;\n1;2")).to eq(3)
+      end
+    end
+  end
+end
